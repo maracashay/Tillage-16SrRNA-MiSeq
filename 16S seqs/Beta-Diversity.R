b3f0bda3dd007ff2340629b9fed8cc491ecc800c
Mara Cloutier <muc345@psu.edu>
2022-04-01 14:35:09 -0400

    Update Beta-Diversity.R

--- 16S seqs/Beta-Diversity.R
+++ 16S seqs/Beta-Diversity.R
@@ -1,61 +1,33 @@
-#### perform PERMANOVA on full data (crop and soybean) ####
-library(vegan)
-ps.bc <- vegdist(otu_table(ps.rel), method = "bray")
-bc.permanova <- adonis(ps.bc ~ Crop * Till * Type, data = metadata, nperm= 999)
-bc.permanova
+pruned.otu <- data.frame(ps.4@otu_table)
+map.pruned <- data.frame(ps.4@sam_data)
+# transform via center-log ratio
+ps_clr <- microbiome::transform(ps.4, "clr")   
+# calculate distance matrix from CLR transformed ASV table with Euclidean distance
+clr_dist_matrix <- phyloseq::distance(ps_clr, method = "euclidean") 
+#ADONIS test
+bc.perm.core <- adonis(clr_dist_matrix ~ Crop * Type * Till, data = map.pruned)
+bc.perm.core
 
-library(RVAideMemoire)
-pairwise.perm.manova(bc_rt_highcover_ns_abun, map.pruned$Till, nperm = 999)
+pairwise.perm.manova(clr_dist_matrix, map.pruned$Crop_type ,nperm=100)
+pairwise.perm.manova(clr_dist_matrix, map.pruned$Till ,nperm=100)
 
-#### Plotting diversity ordination ####
 
-gpca <- ordinate(ps.rel, "PCoA")
+# Plotting PCoA# 
+p1 <- plot_ordination(ps_clr, gpca, "samples", color = "Date")
+plot_ordination(ps_clr, gpca, "samples", color = "Crop")
 
-p2 <- plot_ordination(ps.rel, gpca, "taxa")
-p1 <- plot_ordination(ps.rel, gpca, "samples", color = "Crop")
-p1
+man.shapes = c(15, 16)
+colvecC <- c("gray30"  "#56B4E9")
 
-p2.df <- data.frame(p2$data)
-p2.mapping <- data.frame(p2$mapping)
-p1.df <- data.frame(p1$data)
-
-man.shapes = c(0,15,1,16,2,17)
-
+pdf("PCoA-CLR-Compartment-Crop.pdf")
 plot(p1.df$Axis.1, p1.df$Axis.2,
-     xlab = paste("PCoA 1 (11.5%)", sep = ""),
-     ylab = paste("PCoA 2 (9..6%)", sep = ""),pch = 16, cex = 2.0, type = "n", cex.lab = 1.5, cex.axis = 1.2, axes = FALSE)
+     xlab = paste("PCoA 1 (6%)", sep = ""),
+     ylab = paste("PCoA 2 (5%)", sep = ""),pch = 16, cex = 2.0, type = "n", cex.lab = 1.5, cex.axis = 1.2, axes = FALSE)
 axis(side = 1, labels = T, lwd.ticks = 2, cex.axis = 1.2, las = 1)
 axis(side = 2, labels = T, lwd.ticks = 2, cex.axis = 1.2, las = 1)
 abline(h = 0, v = 0, lty = 3)
 box(lwd = 2)
-points(p1.df$Axis.1, p1.df$Axis.2, pch = man.shapes[map.pruned$Crop_Type], cex = 2, bg = "gray", col =c("brown3", "brown4", "coral")[map.pruned$Till])
-legend("topleft", legend = levels(map.pruned$Crop_Type), col =c("brown3", "brown4", "coral")[map.pruned$Till],pch = man.shapes[map.pruned$Crop_Type], cex = 0.7 )
+points(p1.df$Axis.1, p1.df$Axis.2, pch = man.shapes[p1.df$Type], cex = 2, bg = "gray", col = colvecC[p1.df$Crop])
+dev.off()
 
-#### PERMANOVA on either Corn or Soybean ####
-
-ps.soy <- subset_samples(ps.rel, Crop =="Soy")
-soy.meta <- data.frame(sample_data(ps.soy))
-dist.soy <- vegdist(otu_table(ps.soy), method = "bray")
-adonis(dist.soy ~ Stage * Till * Type, data= soy.meta
-
-pairwise.perm.manova(dist.soy, soy.meta$Crop_Type, nperm = 999)
-
-#### plotting ordination for soybean composition ###
-gpca <- ordinate(ps.soy, "PCoA")
-
-p2 <- plot_ordination(ps.soy, gpca, "taxa", color = "Class")
-p1 <- plot_ordination(ps.soy, gpca, "samples", color = "Stage")
-
-p1.df <- data.frame(p1$data)
-p2.df <- data.frame(p2$data)
-soy.meta$Stage_Type <- as.factor(paste(soy.meta$Type, soy.meta$Stage))
-
-plot(p1.df$Axis.1, p1.df$Axis.2,
-     xlab = paste("PCoA 1 (18.6%)", sep = ""),
-     ylab = paste("PCoA 2 (11.8%)", sep = ""),pch = 16, cex = 2.0, type = "n", cex.lab = 1.5, cex.axis = 1.2, axes = FALSE)
-axis(side = 1, labels = T, lwd.ticks = 2, cex.axis = 1.2, las = 1)
-axis(side = 2, labels = T, lwd.ticks = 2, cex.axis = 1.2, las = 1)
-abline(h = 0, v = 0, lty = 3)
-box(lwd = 2)
-points(p1.df$Axis.1, p1.df$Axis.2, pch = man.shapes.2[map.soy$Stage], cex = 2, bg = "gray", col =c("darkolivegreen3","darkolivegreen4")[map.soy$Type])
-legend("topleft", legend = levels(map.soy$Stage_Type), col =c("darkolivegreen3","darkolivegreen4")[map.soy$Type],pch = man.shapes.2[map.soy$Stage], cex = 0.7 )
+# Run the above code for Corn and Soybean years- but instead of 'Crop', use 'Stage' as an effect
